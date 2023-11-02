f6969811fd25cc0d1675dba2163e2c6e6e218c59
Art Vardanyan <akvardanyan99@gmail.com>
2023-11-02 16:32:46 -0400

    get by post id: done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -13,4 +13,18 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    const {id} = req.params
+    Posts.findById(id)
+        .then (post => {
+            if (!post) {
+                return res.status(404).json({message: 'The post with the specified ID does not exist'})
+            }
+            res.status(200).json(post)
+        })
+        .catch ( () => {
+            res.status(500).json({message: 'The post information could not be retrieved'})
+        })
+})
+
 module.exports = router;
