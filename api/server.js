ccda4860ac7845ce5f682efd0a71e979195c442f
Art Vardanyan <akvardanyan99@gmail.com>
2023-11-02 16:25:54 -0400

    get posts done

--- api/server.js
+++ api/server.js
@@ -1,2 +1,10 @@
+const express = require('express')
+const postsRouter = require('./posts/posts-router')
+
+const server = express()
+server.use(express.json())
 // implement your server here
 // require your posts router and connect it here
+server.use('/api/posts', postsRouter)
+
+module.exports = server
